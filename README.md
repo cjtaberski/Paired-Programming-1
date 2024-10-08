c8d718bdf8b33c09cba07fa880351977a9236092
Connor <connorjtaberski@gmail.com>
2024-10-08 17:57:38 -0400

    End of day result for exercise

---
+++ README.md
@@ -0,0 +1,23 @@
+In this application you will create a to-do list application that can do the following:
+
+1. Users should be able to view all of the to-do tasks (Checklist: Freedom. #1 Doing dishes #2 Starting the roomba #3 Folding the cloths. If all 3 steps = true then play music)
+
+2. Users should be able to add a to-do to the list of to-do tasks (Checklist is a living document. )
+
+3. Users should be able to mark a task as completed by crossing it out on click (click or crossout)
+
+4. Users should be able to remove to-do tasks by removing those elements from the DOM (ask instructors later)
+
+5. The page should be aesthetically pleasing (aka be styled)
+
+------------------------------
+RECOMMEDED PATH 
+
+- Start by adding the HTML code. This is like the skeleton of the page, so add a title, list, form, input, buttons, etc...
+- Add your JavaScript
+    - Create a variable that will hold your to-do list items.
+    - Add a function that takes in a string as input and adds it to the to-do list.
+    - Add an event handler to populate an HTML element with your to-do list when the user clicks the appropriate button.
+    - Etc... break down the big problems into baby steps and solve them systematically. Test as you go.
+- With the main functionality in place, style your HTML elements using CSS. Make use of the developer console for testing styling.
+- Finished with the main objectives? Continue on to the stretch goals to make your application even better.
--- index.js
+++ index.js
@@ -1,3 +1,123 @@
-var logStatement = document.querySelectorAll('h1');
+//let input = document.querySelector('#addItem')
+//console.log(input);
 
-console.log(logStatement);
+//input.addEventListener("Click" () =>
+
+//event listener sees click
+//executes function that creates a new <li> item in the ordered list whose value is equal to the input, then adds a break statement
+
+//Add your JavaScript
+//- Create a variable that will hold your to-do list items.
+//- Add a function that takes in a string as input and adds it to the to-do list.
+//- Add an event handler to populate an HTML element with your to-do list when the user clicks the appropriate button.
+
+//let listItem = document.querySelectorAll("li");
+//listItems.forEach(item =>{
+
+//item.eventListener("Click", function(item) {
+//    item.value = `<strike>${item.value}<strike>`
+//}
+
+//-----------google---------------
+const todoItems = document.querySelectorAll('.todo-item');
+
+todoItems.forEach(item => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('strikethrough');
+  })
+});
+//----css code
+// .strikethrough {
+//     text-decoration: line-through;
+//   }
+
+// let button = document.querySelector('button');
+
+let button = document.querySelector('button');
+
+let toDoItems = document.querySelectorAll('li')
+
+let container = document.querySelector('ol');
+
+console.log(document.getElementById('newItem').innerHTML);
+
+let value = 3;
+// button.addEventListener("click", () => {
+//     let newItem = document.getElementById('newItem');
+//     let newItemText = newItem.value;
+//     console.log(newItemText);
+// });
+// var startingValue = toDoItems.length - 1;
+
+button.addEventListener("click", () => {
+    console.log('Button has been clicked');
+    let newDiv = document.createElement('div')
+    container.appendChild(newDiv)
+    let newLi = document.createElement('li');
+    let newItem = document.getElementById('newItem');
+    let newItemText = newItem.value;
+    let content = document.createTextNode(newItemText);
+    newLi.appendChild(content);
+    newLi.setAttribute('id', `item4`);
+    let newBox = document.createElement('input');
+    newBox.setAttribute('type', 'checkbox')
+    value++;
+    newBox.setAttribute('id', `item${value}`)
+    newDiv.appendChild(newLi);
+    newDiv.appendChild(newBox);
+    //container.appendChild('br');
+  });
+
+//when a list item is clicked, that list items value is set equal to the original value inclosed in a <strike> tag
+//newLi.appendChild(newBox);
+//newLi.appendChild(document.createTextNode(' ' + newItemText));
+
+
+// //---------google--------------
+// // Get the checkbox element
+// const checkbox = document.getElementById("dog"); 
+
+// // Add an event listener for the 'change' event
+// checkbox.addEventListener("change", function() {
+//   if (this.checked) {
+//     // Checkbox is checked
+//     console.log("Good job walking dog"); 
+//   } else {
+//     // Checkbox is unchecked
+//     console.log("Checkbox is unchecked"); 
+//   }
+// });
+
+
+
+// if all values = true 
+//     then play music 
+// else 
+//     do nothing
+
+{
+    
+    
+    
+    
+    
+    /* <form>
+        <fieldset>
+            <legend>Do thus quest for thine weekend succus</legend>
+            <div>
+                <input type="checkbox" id="dog">
+                <label for="dog">Walk the dog</label><br>
+            </div>    
+            <div>
+                <input type="checkbox" id="roomba">
+                <label for="roomba">Start the roomba</label><br>
+            </div>
+            <div>
+                <input type="checkbox" id="laundry">
+                <labal for='laundry'>Fold the laundry</labal><br>
+            </div>
+            <div> 
+                <input type="checkbox" id="dishes">
+                <label for="dishes">Do the dishes</label><br>
+            </div>   
+        </fieldset> */}
